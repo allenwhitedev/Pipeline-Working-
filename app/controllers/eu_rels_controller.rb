911aedbf049162244066362189af4b3d84348880
allenwhitedev <allenwhitedev@gmail.com>
2014-10-24 00:24:52 -0400

    Add code check to checkin form(field right above button)

--- app/controllers/eu_rels_controller.rb
+++ app/controllers/eu_rels_controller.rb
@@ -3,8 +3,14 @@ class EuRelsController < ApplicationController
 
   def create
     @event = Event.find(params[:eu_rel][:attended_id])
+    if codes_match?
     current_user.attend!(@event)
+    flash[:success] = "You are now checked in to this event"
     redirect_to @event
+    else
+    flash[:danger] = "The checkin code does not match"
+    redirect_to @event
+    end
   end
 
   def destroy
@@ -12,4 +18,9 @@ class EuRelsController < ApplicationController
     current_user.unattend!(@event)
     redirect_to @event
   end
+
+  def codes_match?
+    params[:code_attempt] == @event.checkincode
+  end
+  
 end
