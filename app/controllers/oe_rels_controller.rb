d087e87fb94cb484cbe992ec2a2177e3e1bddbdf
allenwhitedev <allenwhitedev@gmail.com>
2014-10-31 01:11:53 -0400

    Setup organization-event controller

---
+++ app/controllers/oe_rels_controller.rb
@@ -0,0 +1,18 @@
+class OeRelsController < ApplicationController
+  before_action :signed_in_user
+
+  def create
+    #?@event = Event.find(params[:id])
+    @organization = Organization.find(params[:oe_rel][:attended_id])
+    @event.attend!(@organization) #current_event could be created and used here possibly
+    redirect_to @event
+  end
+
+  def destroy
+    #?@event = Event.find(params[:id])
+    @organization = Organization.find(params[:id]).attended
+    @event.unattend!(@organization) #current_event could be created and used here possibly
+    redirect_to @event
+  end
+  
+end
